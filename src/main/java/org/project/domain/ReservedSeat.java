231cc6f2ca363b72e7f55e2d3a4ca07f8300ddc1
sandorszucs <szucs_sandor_lma@yahoo.com>
2019-01-23 22:48:17 +0200

    ReservedSeat class created

---
+++ src/main/java/org/project/domain/ReservedSeat.java
@@ -0,0 +1,22 @@
+package org.project.domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "reservedSeats")
+public class ReservedSeat {
+
+    @Id
+    @Column(name = "Id")
+    @GeneratedValue(generator = "reservedSeats_generator")
+    @SequenceGenerator(
+            name = "reservedSeats_generator",
+            sequenceName = "reservedSeats_sequence",
+            initialValue = 1
+    )
+    private long id;
+    @Column (name = "seat")
+    private Seat seat;
+
+
+}
