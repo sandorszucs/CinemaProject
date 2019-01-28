3d9bf291ad50fe1f19c17ada289baf0cd98995cf
sandorszucs <szucs_sandor_lma@yahoo.com>
2019-01-28 16:05:42 +0200

    CinemaRepository and CinemaDTO deleted

--- src/main/java/org/project/persistence/CinemaRepository.java
+++
@@ -1,9 +0,0 @@
-package org.project.persistence;
-
-import org.project.domain.Cinema;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-
-public interface CinemaRepository extends
-        PagingAndSortingRepository<Cinema, Long> {
-}
