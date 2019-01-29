7d6d721fdf31707f3dabf0fa8546277f7bbaaa0d
sandorszucs <szucs_sandor_lma@yahoo.com>
2019-01-29 20:27:10 +0200

    Enable cross server communication

--- src/main/java/org/project/CinemaApplication.java
+++ src/main/java/org/project/CinemaApplication.java
@@ -1,11 +1,25 @@
 package org.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class CinemaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CinemaApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
     }
 }
