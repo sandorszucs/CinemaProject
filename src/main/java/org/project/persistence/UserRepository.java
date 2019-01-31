710b77a169c65fb026d4959f0fd25aec322931c7
sandorszucs <szucs_sandor_lma@yahoo.com>
2019-01-31 20:15:18 +0200

    findBy : FirstName, LastName & Email

--- src/main/java/org/project/persistence/UserRepository.java
+++ src/main/java/org/project/persistence/UserRepository.java
@@ -6,4 +6,8 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface UserRepository extends
         PagingAndSortingRepository <User, Long> {
+
+    User findByFirstName(String firstName);
+    User findByLastName (String lastName);
+    User findByEmail (String email);
 }
