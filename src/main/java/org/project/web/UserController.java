648c1c7a0f736a69ab8683b2624e7071bcb57900
sandorszucs <szucs_sandor_lma@yahoo.com>
2019-01-31 20:16:53 +0200

    UserController CRUD operations

--- src/main/java/org/project/web/UserController.java
+++ src/main/java/org/project/web/UserController.java
@@ -1,6 +1,6 @@
 package org.project.web;
 
-import org.project.domain.User;
+import org.project.dto.UserDTO;
 import org.project.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,10 +17,36 @@ public class UserController {
     private UserService userService;
 
     @RequestMapping(path = {"/{userId}"}, method = RequestMethod.GET)
-    public User findUser(@PathVariable("userId") long userId) {
+    public UserDTO findUser(@PathVariable("userId") long userId) {
         LOGGER.info("userId >> {}", userId);
-
-
-        return userService.getUserbyId(userId);
+        return userService.getUserById(userId);
     }
+
+//    @RequestMapping(path = "/user/{id}", method = RequestMethod.GET)
+//    public UserDTO getUser(@PathVariable("id") long id){
+//
+//        return userService.getUserById(id);
+//    }
+
+
+    @RequestMapping(path = "/user", method = RequestMethod.GET)
+    public UserDTO getUser(@RequestParam("name") String email){
+        return userService.getUserByEmail(email);
+    }
+
+    @RequestMapping(path="/user", method=RequestMethod.POST)
+    public void saveUser(@RequestBody UserDTO userDTO){
+        userService.saveUser(userDTO);
+    }
+
+    @RequestMapping(path="/user/{id}", method=RequestMethod.PUT)
+    public UserDTO updateUser(@PathVariable long id, @RequestBody UserDTO dto){
+        return userService.updateUser(id, dto);
+    }
+
+    @RequestMapping(path = "/user/{id}", method =RequestMethod.DELETE)
+    public void deleteUser(@PathVariable("id") long id){
+        userService.deleteUserById(id);
+    }
+
 }
