e519f4fff687fc2a7df9882afc481f5b08ee8775
sandorszucs <szucs_sandor_lma@yahoo.com>
2019-02-07 15:44:42 +0200

    Reservation & Schedule Controller looks ok! MUST BE CHECKED!

--- src/main/java/org/project/web/ReservationController.java
+++ src/main/java/org/project/web/ReservationController.java
@@ -5,8 +5,6 @@ import org.project.service.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
-
 @RestController
 public class ReservationController {
 
--- src/main/java/org/project/web/ScheduleController.java
+++ src/main/java/org/project/web/ScheduleController.java
@@ -5,33 +5,30 @@ import org.project.service.ScheduleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
-
 @RestController
 public class ScheduleController {
 
     @Autowired
     private ScheduleService scheduleService;
 
-//    @RequestMapping(path = "/schedule/{id}", method = RequestMethod.GET)
-//    public ScheduleDTO getSchedule(@PathVariable("id") {
-//        return scheduleService.getScheduleByDate();
-//    }
 
+    @RequestMapping(path = "/schedule/{id}", method = RequestMethod.GET)
+    public ScheduleDTO getSchedule(@PathVariable("id") long id) {
+        return scheduleService.getScheduleById(id);
+    }
 
-//    @RequestMapping(path = "/schedule", method = RequestMethod.POST)
-//    public void saveRunnedGame(@RequestBody ScheduleDTO scheduleDTO) {
-//        scheduleService.save (scheduleDTO);
-//    }
+    @RequestMapping(path = "/schedule", method = RequestMethod.POST)
+    public void saveSchedule(@RequestBody ScheduleDTO scheduleDTO) {
+        scheduleService.saveSchedule(scheduleDTO);
+    }
 
+    @RequestMapping(path = "/schedule/{id}", method = RequestMethod.PUT)
+    public ScheduleDTO updateSchedule(@PathVariable long id, @RequestBody ScheduleDTO dto) {
+        return scheduleService.updateSchedule(id, dto);
+    }
 
-//    @RequestMapping(path = "/schedule/{id}", method = RequestMethod.PUT)
-//    public ScheduleDTO updateSchedule(@PathVariable long id, @RequestBody ScheduleDTO dto) {
-//        return scheduleService.updateSchedule(id, dto);
-//    }
-//
-//    @RequestMapping(path = "/schedule/{id}", method = RequestMethod.DELETE)
-//    public void deleteSchedule(@PathVariable("id") long id) {
-//        ScheduleService.deleteScheduleById(id);
-//    }
+    @RequestMapping(path = "/schedule/{id}", method = RequestMethod.DELETE)
+    public void deleteSchedule(@PathVariable("id") long id) {
+        scheduleService.deleteScheduleById(id);
+    }
 }
