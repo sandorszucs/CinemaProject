d57a54c4af7f5152aa34d144815a744d67860a28
sandorszucs <szucs_sandor_lma@yahoo.com>
2019-02-20 22:45:51 +0200

    FindUserById

--- src/main/java/org/project/persistence/UserRepository.java
+++ src/main/java/org/project/persistence/UserRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface UserRepository extends PagingAndSortingRepository <User, Long> {
 
-    User findByFirstName(String firstName);
-    User findByLastName (String lastName);
+    User findUserById (long id);
     User findByEmail (String email);
 }
