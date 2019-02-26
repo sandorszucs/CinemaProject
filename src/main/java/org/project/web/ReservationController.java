e95cbb6ee6b3e07563b6ad1acf71bbcb0af01a64
sandorszucs <szucs_sandor_lma@yahoo.com>
2019-02-27 00:01:48 +0200

    Controllers tested with Postman & Working

--- src/main/java/org/project/web/ReservationController.java
+++ src/main/java/org/project/web/ReservationController.java
@@ -1,9 +1,13 @@
 package org.project.web;
 
 import org.project.dto.ReservationDTO;
+import org.project.dto.SeatDTO;
 import org.project.service.ReservationService;
+import org.project.service.ScheduleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ReservationController {
--- src/main/java/org/project/web/ScheduleController.java
+++ src/main/java/org/project/web/ScheduleController.java
@@ -3,6 +3,7 @@ package org.project.web;
 import org.project.domain.Schedule;
 import org.project.dto.HallDTO;
 import org.project.dto.ScheduleDTO;
+import org.project.dto.SeatDTO;
 import org.project.service.ScheduleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -25,15 +26,16 @@ public class ScheduleController {
         return scheduleService.getSchedules();
     }
 
-//    @RequestMapping(path = "/schedule", method = RequestMethod.GET)
-//    public List<HallDTO> getHalls(){
-//        return scheduleService.getHalls();
-//    }
+    @RequestMapping(path = "/schedule/halls", method = RequestMethod.GET)
+    public List<HallDTO> getHalls(){
+        return scheduleService.getHalls();
+    }
 
-//    @RequestMapping(path = "/reservation", method = RequestMethod.GET)
-//    public Schedule getAllAvailableSeatForScheduledId (@RequestParam long scheduleID) {
-//        return scheduleService.getAllAvailableSeat(scheduleID);
-//    }
+
+    @RequestMapping(path = "/schedule/{id}/seats", method = RequestMethod.GET)
+    public List<SeatDTO> getAllAvailableSeatForScheduledId (@PathVariable("id")  long id) {
+        return scheduleService.getAllAvailableSeat(id);
+    }
 
 
     @RequestMapping(path = "/schedule", method = RequestMethod.POST)
